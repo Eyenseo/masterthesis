31ae8fea12a929bc73c26340a06bacb90fffe49d
eyenseo <eyenseo@gmail.com>
2018-10-10 00:24:35 +0200

    Layout

--- data/v64_dec_new.cpp
+++ data/v64_dec_new.cpp
@@ -1,13 +1,16 @@
-int64_t decode() {
+inline int64_t decode() {
   uint64_t v;
 
   if ((v = *(position++) >= 0x80U)) {
     v = (v & 0x80U - 1) | (*(position++) << 7U);
-    // ...
+    if (v >= 0x4000U) {
+      v = (v & 0x4000U - 1) | (*(position++) << 14U);
+      // ...
                 if (v >= 0x100000000000000U) {
                   v = (v & 0x100000000000000U - 1) | (*(position++)<<56U);
                 }
-    // ...
+      // ...
+    }
   }
   return v;
 }
--- data/v64_enc_new.cpp
+++ data/v64_enc_new.cpp
@@ -1,15 +1,20 @@
-void encode(uint64_t v) {
+inline void encode(uint64_t v) {
   if (v < 0x80U) {
     *(position++) = (uint8_t)(v);
   } else {
     *(position++) = (uint8_t)(0x80U | v);
-    // ...
+    if (v < 0x4000U) {
+      *(position++) = (uint8_t)(v >> 7U);
+    } else {
+      *(position++) = (uint8_t)(0x80U | v >> 7U);
+      // ...
                 if (v < 0x100000000000000U) {
                   *(position++) = (uint8_t)(v >> 49U);
                 } else {
                   *(position++) = (uint8_t)(0x80U | v >> 49U);
                   *(position++) = (uint8_t)(v >> 56U);
                 }
-    // ...
+      // ...
+    }
   }
 }
